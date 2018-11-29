93640e790b74e7c34fbc647fb851457bcf507322
Klayton Faria <klayton.faria@farfetch.com>
2018-11-29 15:00:48 +0000

    chore(docz): Update config

---
+++ doczrc.js
@@ -0,0 +1,22 @@
+export default {
+  dest: './public',
+  base: '/',
+  hashRouter: true,
+  wrapper: 'wrapper.js',
+  codeSandbox: false,
+  files: './docs/**/*.mdx',
+  themeConfig: {
+    logo: {
+      src: 'https://instagram.flis8-2.fna.fbcdn.net/vp/7d2e51b014efca981d3a8eb4bf5c3d07/5C916DBF/t51.2885-19/s320x320/14482113_1143788649039310_7192075474778980352_a.jpg',
+      width: 160,
+      height: 160
+    },
+    colors: {
+      primary: '#1B1D1C',
+      text: '#1B1D1C',
+      sidebarBg: '#FAFAFA',
+      background: '#FAFAFA',
+      link: '#1fb6ff'
+    }
+  }
+}
