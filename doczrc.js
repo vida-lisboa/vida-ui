81e2063cc7eeec26dfa284ea6f7d309b1228c505
Klayton Faria <klayton.souza@gmail.com>
2018-12-05 23:51:55 +0000

    chore(Docz): Update source path

--- doczrc.js
+++ doczrc.js
@@ -3,7 +3,6 @@ export default {
   base: '/',
   hashRouter: true,
   codeSandbox: false,
-  files: './docs/**/*.mdx',
   themeConfig: {
     logo: {
       src: 'https://instagram.flis8-2.fna.fbcdn.net/vp/7d2e51b014efca981d3a8eb4bf5c3d07/5C916DBF/t51.2885-19/s320x320/14482113_1143788649039310_7192075474778980352_a.jpg',
