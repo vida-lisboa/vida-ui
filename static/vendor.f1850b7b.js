a3bb5da18f78bdb7454e0c8657c9d47a73b8d8de
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-03-13 19:36:53 +0000

    Deploy ui.vidalisboa.pt to github.com/vida-lisboa/vida-ui.git:gh-pages

--- static/vendor.f1850b7b.js
+++ static/vendor.f1850b7b.js
@@ -1,4 +1,4 @@
-webpackJsonp([5],{
+webpackJsonp([6],{
 
 /***/ 707:
 /***/ (function(module, exports, __webpack_require__) {
@@ -40,4 +40,4 @@ valueOf:!1,WeakMap:!1,WeakSet:!1},es5:{Array:!1,Boolean:!1,constructor:!1,Date:!
 /***/ })
 
 });
-//# sourceMappingURL=vendor.80cb7d33.js.map
+//# sourceMappingURL=vendor.f1850b7b.js.map
